5a774d174f0db970adc2ff324ef3333197d59038
Daniel Holth <dholth@fastmail.fm>
2012-07-23 22:16:30 -0400

    additional files needed for ref10 to work by itself

--- ed25519-supercop-ref10/keypair.c
+++ ed25519-supercop-ref10/keypair.c
@@ -1,16 +1,15 @@
-#include "randombytes.h"
 #include "crypto_sign.h"
 #include "crypto_hash_sha512.h"
 #include "ge.h"
 
-int crypto_sign_keypair(unsigned char *pk,unsigned char *sk)
+
+int crypto_sign_keypair(unsigned char *pk,unsigned char *sk,unsigned char *seed)
 {
   unsigned char h[64];
   ge_p3 A;
   int i;
 
-  randombytes(sk,32);
-  crypto_hash_sha512(h,sk,32);
+  crypto_hash_sha512(h,seed,32);
   h[0] &= 248;
   h[31] &= 63;
   h[31] |= 64;
@@ -19,5 +18,6 @@ int crypto_sign_keypair(unsigned char *pk,unsigned char *sk)
   ge_p3_tobytes(pk,&A);
 
   for (i = 0;i < 32;++i) sk[32 + i] = pk[i];
+  for(i=0;i<32;i++) sk[i] = seed[i];
   return 0;
 }
